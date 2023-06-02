0f9ddd39ec57d7d91f990758605acd6fc406a1b2
Danyil Suhak <danyil.suhak@cdev.com.ua>
2023-06-02 15:23:33 +0300

    Added test_id for ResultOption db model.

--- backend/cw/database/models/test/ResultOption.py
+++ backend/cw/database/models/test/ResultOption.py
@@ -1,13 +1,14 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 
 from .meta import BaseTest
-from .QuestionCategory import QuestionCategory
+from .QuestionCategory import QuestionCategory, Test
 
 
 class ResultOption(BaseTest):
     __tablename__ = "result_option"
 
     id = Column(Integer(), primary_key=True)
+    test_id = Column(Integer, ForeignKey(Test.id), nullable=False)
     question_category_id = Column(Integer, ForeignKey(QuestionCategory.id), nullable=False)
     min = Column(Integer(), nullable=False)
     max = Column(Integer(), nullable=False)
