4d1c2c2d542958cdb305a586c83089ad6fb441f0
Danyil Suhak <danyil.suhak@cdev.com.ua>
2023-06-06 22:52:18 +0300

    Increase question_category.description max length. Corrected alembic configs.

--- backend/cw/database/models/test/QuestionCategory.py
+++ backend/cw/database/models/test/QuestionCategory.py
@@ -10,7 +10,7 @@ class QuestionCategory(BaseTest):
     id = Column(Integer(), primary_key=True)
     test_id = Column(Integer, ForeignKey(Test.id), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
+    description = Column(String(2048), nullable=False)
 
     def __repr__(self):
         return f"QuestionCategory({self.id}, {self.test_id})"
