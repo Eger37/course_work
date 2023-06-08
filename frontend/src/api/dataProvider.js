0ed113ec37be86ccc5edc9f2e38e1da41860bbb2
Danyil Suhak <danyil.suhak@cdev.com.ua>
2023-06-08 12:39:02 +0300

    data provider

--- frontend/src/api/dataProvider.js
+++ frontend/src/api/dataProvider.js
@@ -1,8 +1,8 @@
 import {httpClient} from "./httpClient";
 
 
-export const getTest = (testId) => {
-    return httpClient(`${testId}`, {
+export const getOne = (url) => {
+    return httpClient( url,{
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -18,16 +18,17 @@ export const getTest = (testId) => {
             return res.json()
         }
     }).catch(function () {
-        console.log("error getTest");
+        console.log("error getOne");
     });
 };
 
 
-export const getTests = () => {
-    return httpClient("test", {
+export const getList = (url) => {
+    return httpClient(url, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
+            "Accept": "application/json",
         },
     }).then(res => {
         if (res.status !== 200) {
@@ -43,6 +44,27 @@ export const getTests = () => {
     });
 };
 
+export const createOne = (url, data) => {
+    return httpClient(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({...data}),
+    }).then(res => {
+        if (res.status !== 200) {
+            res.json().then((data) => {
+                alert(data.message)
+            })
+            return Promise.reject(res.status !== 200)
+        } else {
+            return res.json()
+        }
+    }).catch(function () {
+        console.log("error");
+    });
+};
 
 export const postTest1 = ({userId, test1Data, answers}) => {
     return httpClient("test1", {
