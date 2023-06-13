7c899600d2fe74fba8b236bb7a0a8dd50cb3cab8
Danyil Suhak <danyil.suhak@cdev.com.ua>
2023-06-13 19:41:15 +0300

    Added resources for psychologist.

--- frontend_admin/src/app/roles/psychologist.js
+++ frontend_admin/src/app/roles/psychologist.js
@@ -9,5 +9,9 @@ export const psychologistResources = [
     <Resource {...clientsResource} name="client"/>,
     <Resource {...testResource} name="test"/>,
     <Resource name="user_tests" intent={"registration"}/>,
+    <Resource name="question-category" intent={"registration"}/>,
+    <Resource name="question" intent={"registration"}/>,
+    <Resource name="result-option" intent={"registration"}/>,
+    <Resource name="answer-option" intent={"registration"}/>,
 
 ];
