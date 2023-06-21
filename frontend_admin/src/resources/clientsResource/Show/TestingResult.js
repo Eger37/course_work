a5c6895cda4e3bb6d6dc0013670afb0b75cc424f
Danyil Suhak <danyil.suhak@cdev.com.ua>
2023-06-21 17:55:24 +0300

    Added Testing result

--- frontend_admin/src/resources/clientsResource/Show/TestingResult.js
+++ frontend_admin/src/resources/clientsResource/Show/TestingResult.js
@@ -6,10 +6,11 @@ import {
 } from 'react-admin';
 import {useParams, Redirect} from "react-router-dom"
 
-import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import {GridForm, GridInput} from '../../../components/GridForm';
@@ -20,44 +21,42 @@ import {TestingsField} from "./TestingsField";
 import {ActionsWithBackButton} from "./Show";
 
 
-// export const EditTestingForm = ({handleSubmit, handleClose, loading, testing}) => (
-//     <FormWithRedirect
-//         record={testing}
-//         component={DialogContent}
-//         save={handleSubmit}
-//         render={({handleSubmitWithRedirect}) => (
-//             <React.Fragment>
-//                 <GridForm>
-//                     <GridInput xs={12} component={TextInput} label="Note" source="note"
-//                                multiline/>
-//                 </GridForm>
-//                 <DialogActions>
-//                     <Button disabled={loading} onClick={handleClose} color="primary">
-//                         Cancel
-//                     </Button>
-//                     <Button disabled={loading} onClick={handleSubmitWithRedirect} color="primary">
-//                         Update
-//                     </Button>
-//                 </DialogActions>
-//             </React.Fragment>
-//         )}
-//     />
-// )
+export function CategoryResult({categoryResult}) {
+
+    return (
+        <Paper
+            style={{
+                marginBottom: 10,
+                mx: 'auto',
+                padding: 10,
+            }}
+        >
+            <Grid container wrap="nowrap" spacing={2}>
+                <Grid item xs zeroMinWidth>
+                    <Typography variant="h5" gutterBottom>{categoryResult.question_category.name}</Typography>
+
+                    <Typography gutterBottom>{categoryResult.question_category.description}</Typography>
+                    <Typography gutterBottom style={{fontWeight: 'bold'}}>
+                        Набрано балів у категорії: {categoryResult.score}
+                    </Typography>
+                    <Typography gutterBottom style={{fontWeight: 'bold'}}>
+                        {categoryResult.result_option.text}
+                    </Typography>
+
+                </Grid>
+            </Grid>
+        </Paper>
+    );
+}
 
 export const TestingResult = (props) => {
-    const notifyError = useNotifyError();
     const {clientId, testingId} = useParams();
-    const [redirect, setRedirect] = React.useState(false);
     const {loaded, error, data} = useQuery({
         type: 'getOne',
         resource: 'testing-result',
         payload: {id: testingId}
     });
 
-
-    const handleClose = () => {
-        setRedirect(true);
-    }
     if (!loaded) {
         return <Loading/>;
     }
@@ -65,31 +64,26 @@ export const TestingResult = (props) => {
         return <Error/>;
     }
     return (
-        <Paper evaluation={3}>
-            {/*<div>*/}
+        <Show actions={<ActionsWithBackButton hasList={true} hasEdit={true}/>}
+              basePath={`/client/${clientId}/show/`}
+              id={testingId}
+              record={data}
+              resource={"testing-result"}
+        >
+            <SimpleShowLayout style={{paddingBottom: 8}}>
+                <center>
+                    <Typography variant="h4" noWrap>Результат</Typography>
+                </center>
 
-            {/*    {data.testing_result_for_category}*/}
+                {data.testing_result_for_category.map((categoryResult) => (
 
-            {/*</div>*/}
-            dsadas
+                    <Grid item key={categoryResult.question_category.id} sm={12}>
+                        <CategoryResult categoryResult={categoryResult}/>
+                    </Grid>
+                ))}
 
-            {/*<Show actions={<ActionsWithBackButton hasEdit={true}/>} {...props}>*/}
-            {/*    <SimpleShowLayout style={{paddingBottom: 8}}>*/}
-            {/*        <TextField source="anamnesisFromPsychologist" label={"Анамнез"}/>*/}
-            {/*        <TestingsField label="Tests" reference="testing" target="user_id"/>*/}
-            {/*    </SimpleShowLayout>*/}
-            {/*</Show>*/}
-            {/*// <Paper evaluation={3}>*/}
-            {/*//     <Box  sx={{ p: 2 }}>*/}
-            {/*//         {redirect && <Redirect to={`/client/${clientId}/show/`}/>}*/}
-            {/*//         <h1>Edit testing {testingId}</h1>*/}
-            {/*//         <EditTestingForm*/}
-            {/*//                           handleClose={handleClose}*/}
-            {/*//                           loading={loading}*/}
-            {/*//                           testing={data}*/}
-            {/*//         />*/}
-            {/*//     </Box>*/}
-            {/*//*/}
-        </Paper>
+            </SimpleShowLayout>
+        </Show>
+
     );
 };
